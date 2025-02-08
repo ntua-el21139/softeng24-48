4e1eb130b8f63003ceeae40fabe7fb82a53caace
ntua-el21054git config --global user.email el21054@mail.ntua.gr <el21054@mail.ntua.gr>
2025-02-08 22:36:07 +0200

    front-end bug fix

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +9,11 @@ export default defineConfig({
   build: {
     outDir: "build"
   },
-  plugins: [tsconfigPaths(), react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      'lib': path.resolve(__dirname, './src/lib'),
+      'components': path.resolve(__dirname, './src/components')
+    }
+  }
 });
