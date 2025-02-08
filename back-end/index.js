6e3b0a60f2f6d67517b2b1946c580494334cd856
ntua-el21139 <el21139@mail.ntua.gr>
2025-02-08 13:42:53 +0200

    first endpoint

--- back-end/index.js
+++ back-end/index.js
@@ -10,6 +10,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//Import and register the tollStationPasses route
+const tollStationPassesRoutes = require('./routes/endpoints/tollStationPasses');
+app.use('/api/tollStationPasses', tollStationPassesRoutes);
+
 // Basic route for testing
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the API!' });
