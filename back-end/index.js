c0ee23f3d0170573154e9017e46d68ecc4bb4afb
ntua-el21139 <el21139@mail.ntua.gr>
2025-02-09 19:43:34 +0200

    changed port to 9115

--- back-end/index.js
+++ back-end/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const routes = require('./routes');  // Import the centralized router
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9115;
 
 // Middleware
 app.use(cors());
