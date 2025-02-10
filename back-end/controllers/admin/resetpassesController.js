a08e6e35adbaf8b16e87c917e29f46b2f1759545
ggfiogkos69 <christsourvel@gmail.com>
2025-02-10 18:14:51 +0200

    fuck git #2

--- back-end/controllers/admin/resetpassesController.js
+++ back-end/controllers/admin/resetpassesController.js
@@ -1,31 +1,20 @@
 const pool = require('../../utils/database');
 
 exports.resetpasses = async (req, res) => {
-    const connection = await pool.getConnection();
-    
     try {
-        await connection.beginTransaction();
-        
-        // Delete all records
+        const connection = await pool.getConnection();
         await connection.query('DELETE FROM Passes');
-        
-        // Reset auto-increment counter
-        await connection.query('ALTER TABLE Passes AUTO_INCREMENT = 1');
-        
-        await connection.commit();
+        connection.release();
 
         res.json({
             status: "OK"
         });
     } catch (error) {
-        await connection.rollback();
         console.error("Database error:", error);
         res.status(500).json({
             status: "failed",
             info: error.message
         });
-    } finally {
-        connection.release();
     }
 };
 
