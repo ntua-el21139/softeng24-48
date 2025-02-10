c73613d07da62ce3e7ddaba25aa6d56008311fe6
ntua-el21139 <el21139@mail.ntua.gr>
2025-02-10 18:09:20 +0200

    fixed reset passes

--- back-end/controllers/admin/resetpassesController.js
+++ back-end/controllers/admin/resetpassesController.js
@@ -1,20 +1,31 @@
 const pool = require('../../utils/database');
 
 exports.resetpasses = async (req, res) => {
+    const connection = await pool.getConnection();
+    
     try {
-        const connection = await pool.getConnection();
+        await connection.beginTransaction();
+        
+        // Delete all records
         await connection.query('DELETE FROM Passes');
-        connection.release();
+        
+        // Reset auto-increment counter
+        await connection.query('ALTER TABLE Passes AUTO_INCREMENT = 1');
+        
+        await connection.commit();
 
         res.json({
             status: "OK"
         });
     } catch (error) {
+        await connection.rollback();
         console.error("Database error:", error);
         res.status(500).json({
             status: "failed",
             info: error.message
         });
+    } finally {
+        connection.release();
     }
 };
 
